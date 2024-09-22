32e431e079969aa86bc9bf3327ad6be75c7c09a9
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-09-21 22:27:30 -0700

    Refactor streamlit_app.py to handle user input for Anilist username and fetch data with selected statuses

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -14,20 +14,23 @@ st.write(
 anilist_user_input = st.text_input("Enter your Anilist username (leave blank to use default):", "")
 anilist_user = anilist_user_input if anilist_user_input else anilist_user_default
 
+status_options = ["COMPLETED", "PLANNING", "CURRENT"]
+selected_statuses = st.multiselect("Select the list you want to fetch:", status_options)
+
 if st.button("Fetch Data"):
     if anilist_user:
-        st.write(f"Fetching data for user: {anilist_user}...")
-        data_frames, anilist_user = fetch_and_save_data(anilist_user, anilist_user_default)
+        st.write(f"Fetching data for user: {anilist_user} for statuses: {', '.join(selected_statuses)}...")
+        data_frames, anilist_user = fetch_and_save_data(anilist_user, anilist_user_default, selected_statuses)
 
         if data_frames:
             st.success("Data has been fetched successfully!")
 
             for i, df in enumerate(data_frames):
-                status = ["COMPLETED", "PLANNING", "CURRENT"][i]
+                status = selected_statuses[i]
                 csv = df.to_csv(index=False).encode("utf-8")
 
                 st.download_button(
-                    label=f"Download {anilist_user}'s ({status} anime data)",
+                    label=f"Download {anilist_user}'s ({status}) anime data",
                     data=csv,
                     file_name=f"anilist_{anilist_user}_{status}_anime.csv",
                     mime="text/csv",
