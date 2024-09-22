e84ef99f220290461cc4f68d5a479ba5d6c22abe
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-09-21 22:19:17 -0700

    Refactor streamlit_app.py to handle user input for Anilist username and fetch data

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -3,21 +3,17 @@ import streamlit as st
 from app_utils import fetch_and_save_data
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 anilist_user_default = os.getenv("ANILIST_USER")
 
-# Intro page
 st.title("Anime Analysis")
 st.write(
     "Welcome to the Anime Analysis Streamlit App! This app allows you to fetch and analyze your Anilist anime data."
 )
 
-# User input for Anilist username
 anilist_user_input = st.text_input("Enter your Anilist username (leave blank to use default):", "")
 anilist_user = anilist_user_input if anilist_user_input else anilist_user_default
 
-# Fetch data button
 if st.button("Fetch Data"):
     if anilist_user:
         st.write(f"Fetching data for user: {anilist_user}...")
@@ -27,8 +23,8 @@ if st.button("Fetch Data"):
             st.success("Data has been fetched successfully!")
 
             for i, df in enumerate(data_frames):
-                status = ["COMPLETED", "PLANNING", "CURRENT"][i]  # Adjust based on your statuses
-                csv = df.to_csv(index=False).encode("utf-8")  # Convert DataFrame to CSV
+                status = ["COMPLETED", "PLANNING", "CURRENT"][i]
+                csv = df.to_csv(index=False).encode("utf-8")
 
                 st.download_button(
                     label=f"Download {anilist_user}'s ({status} anime data)",
