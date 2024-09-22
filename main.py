75bd7c229e022a9e6c92464bc62c1b0b75e4382e
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-09-21 18:46:56 -0700

    Refactor main.py to remove unnecessary code and improve code structure

--- main.py
+++ main.py
@@ -5,89 +5,38 @@ from dotenv import load_dotenv
 
 from src.data_processor import process_data, process_metadata
 from src.logger import setup_logging
+from src.queries import data_query_template, metadata_query_template
 from src.save_data import save_csv, save_json
 from src.utils import extract_entries, make_request
 
-# Load environment variables from .env
 load_dotenv()
-
-# Set up logging
 setup_logging()
 
-# Fetch the Anilist user from .env
 anilist_user = os.getenv("ANILIST_USER")
-
-# Define query templates
-metadata_query_template = """
-{{
-  MediaListCollection(userName: "{anilist_user}", type: ANIME, status: {status}) {{
-    lists {{
-      entries {{
-        id
-        mediaId
-        score
-      }}
-    }}
-  }}
-}}
-"""
-
-data_query_template = """
-{{
-  MediaListCollection(userName: "{anilist_user}", type: ANIME, status: {status}) {{
-    lists {{
-      entries {{
-        media {{
-          id
-          title {{
-            romaji
-            english
-          }}
-          episodes
-          source
-          genres
-          meanScore
-          description
-          duration
-          season
-          seasonYear
-        }}
-      }}
-    }}
-  }}
-}}
-"""
-
 statuses = ["COMPLETED", "PLANNING", "CURRENT"]
 
 for status in statuses:
     logging.info(f"Fetching data for status: {status}")
 
-    # Format the queries
     metadata_query = metadata_query_template.format(anilist_user=anilist_user, status=status)
     data_query = data_query_template.format(anilist_user=anilist_user, status=status)
 
     try:
-        # Make API requests
+
         metadata_response = make_request(metadata_query)
         data_response = make_request(data_query)
 
-        # Extract entries
         metadata_entries = extract_entries(metadata_response)
         data_entries = extract_entries(data_response)
 
-        # Save JSON files
         save_json(metadata_entries, f"anilist_metadata_{status}.json")
         save_json(data_entries, f"anilist_data_{status}.json")
 
-        # Process the entries into DataFrames
         metadata_df = process_metadata(metadata_entries)
         data_df = process_data(data_entries)
 
-        # Merge metadata and data DataFrames on media_id
         merged_df = metadata_df.merge(data_df, on="media_id")
 
-        # Save merged CSV file
         save_csv(merged_df, f"anilist_merged_{status}.csv")
 
     except Exception as e:
